5fc7b068f721c4e8ac75f0339abba35651120758
venpus <venpus1101@gmail.com>
2026-03-12 14:05:27 +0800

    build error fixed

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,7 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 // 미들웨어 설정
 app.use(helmet({
